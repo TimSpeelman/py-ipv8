b99965504d8bf910ef822355355cb4edc787fe7a
Tim Speelman <timspeelman@live.nl>
2020-07-01 19:56:17 +0200

    fix

--- ipv8/messaging/interfaces/udp/endpoint.py
+++ ipv8/messaging/interfaces/udp/endpoint.py
@@ -56,7 +56,7 @@ class UDPEndpoint(Endpoint, asyncio.DatagramProtocol):
         loop = asyncio.get_event_loop()
 
         for _ in range(10000):
-            try:
+            # try:
                 # It is recommended that this endpoint is opened at port = 0,
                 # such that the OS handles the port assignment
                 self._transport = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
